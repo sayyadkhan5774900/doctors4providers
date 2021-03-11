6f6524da3263ddcf2fcb068f275d65ec3436aa5b
Laravel Daily <info@laraveldaily.com>
2021-03-11 07:36:09 +0000

    QuickAdminPanel automatic commit

---
+++ app/Http/Controllers/Frontend/DoctorController.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\Traits\MediaUploadingTrait;
+use App\Http\Requests\MassDestroyDoctorRequest;
+use App\Http\Requests\StoreDoctorRequest;
+use App\Http\Requests\UpdateDoctorRequest;
+use App\Models\Doctor;
+use Gate;
+use Illuminate\Http\Request;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+use Symfony\Component\HttpFoundation\Response;
+
+class DoctorController extends Controller
+{
+    use MediaUploadingTrait;
+
+    public function index()
+    {
+        abort_if(Gate::denies('doctor_access'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+
+        $doctors = Doctor::with(['media'])->get();
+
+        return view('frontend.doctors.index', compact('doctors'));
+    }
+
+    public function create()
+    {
+        abort_if(Gate::denies('doctor_create'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+
+        return view('frontend.doctors.create');
+    }
+
+    public function store(StoreDoctorRequest $request)
+    {
+        $doctor = Doctor::create($request->all());
+
+        if ($request->input('cv', false)) {
+            $doctor->addMedia(storage_path('tmp/uploads/' . basename($request->input('cv'))))->toMediaCollection('cv');
+        }
+
+        if ($request->input('redacted_cv', false)) {
+            $doctor->addMedia(storage_path('tmp/uploads/' . basename($request->input('redacted_cv'))))->toMediaCollection('redacted_cv');
+        }
+
+        if ($media = $request->input('ck-media', false)) {
+            Media::whereIn('id', $media)->update(['model_id' => $doctor->id]);
+        }
+
+        return redirect()->route('frontend.doctors.index');
+    }
+
+    public function edit(Doctor $doctor)
+    {
+        abort_if(Gate::denies('doctor_edit'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+
+        return view('frontend.doctors.edit', compact('doctor'));
+    }
+
+    public function update(UpdateDoctorRequest $request, Doctor $doctor)
+    {
+        $doctor->update($request->all());
+
+        if ($request->input('cv', false)) {
+            if (!$doctor->cv || $request->input('cv') !== $doctor->cv->file_name) {
+                if ($doctor->cv) {
+                    $doctor->cv->delete();
+                }
+
+                $doctor->addMedia(storage_path('tmp/uploads/' . basename($request->input('cv'))))->toMediaCollection('cv');
+            }
+        } elseif ($doctor->cv) {
+            $doctor->cv->delete();
+        }
+
+        if ($request->input('redacted_cv', false)) {
+            if (!$doctor->redacted_cv || $request->input('redacted_cv') !== $doctor->redacted_cv->file_name) {
+                if ($doctor->redacted_cv) {
+                    $doctor->redacted_cv->delete();
+                }
+
+                $doctor->addMedia(storage_path('tmp/uploads/' . basename($request->input('redacted_cv'))))->toMediaCollection('redacted_cv');
+            }
+        } elseif ($doctor->redacted_cv) {
+            $doctor->redacted_cv->delete();
+        }
+
+        return redirect()->route('frontend.doctors.index');
+    }
+
+    public function show(Doctor $doctor)
+    {
+        abort_if(Gate::denies('doctor_show'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+
+        return view('frontend.doctors.show', compact('doctor'));
+    }
+
+    public function destroy(Doctor $doctor)
+    {
+        abort_if(Gate::denies('doctor_delete'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+
+        $doctor->delete();
+
+        return back();
+    }
+
+    public function massDestroy(MassDestroyDoctorRequest $request)
+    {
+        Doctor::whereIn('id', request('ids'))->delete();
+
+        return response(null, Response::HTTP_NO_CONTENT);
+    }
+
+    public function storeCKEditorImages(Request $request)
+    {
+        abort_if(Gate::denies('doctor_create') && Gate::denies('doctor_edit'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+
+        $model         = new Doctor();
+        $model->id     = $request->input('crud_id', 0);
+        $model->exists = true;
+        $media         = $model->addMediaFromRequest('upload')->toMediaCollection('ck-media');
+
+        return response()->json(['id' => $media->id, 'url' => $media->getUrl()], Response::HTTP_CREATED);
+    }
+}
---
+++ resources/views/frontend/doctors/show.blade.php
@@ -0,0 +1,167 @@
+@extends('layouts.frontend')
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+
+            <div class="card">
+                <div class="card-header">
+                    {{ trans('global.show') }} {{ trans('cruds.doctor.title') }}
+                </div>
+
+                <div class="card-body">
+                    <div class="form-group">
+                        <div class="form-group">
+                            <a class="btn btn-default" href="{{ route('frontend.doctors.index') }}">
+                                {{ trans('global.back_to_list') }}
+                            </a>
+                        </div>
+                        <table class="table table-bordered table-striped">
+                            <tbody>
+                                <tr>
+                                    <th>
+                                        {{ trans('cruds.doctor.fields.id') }}
+                                    </th>
+                                    <td>
+                                        {{ $doctor->id }}
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th>
+                                        {{ trans('cruds.doctor.fields.name') }}
+                                    </th>
+                                    <td>
+                                        {{ $doctor->name }}
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th>
+                                        {{ trans('cruds.doctor.fields.email') }}
+                                    </th>
+                                    <td>
+                                        {{ $doctor->email }}
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th>
+                                        {{ trans('cruds.doctor.fields.phone') }}
+                                    </th>
+                                    <td>
+                                        {{ $doctor->phone }}
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th>
+                                        {{ trans('cruds.doctor.fields.address') }}
+                                    </th>
+                                    <td>
+                                        {{ $doctor->address }}
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th>
+                                        {{ trans('cruds.doctor.fields.practice') }}
+                                    </th>
+                                    <td>
+                                        {{ $doctor->practice }}
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th>
+                                        {{ trans('cruds.doctor.fields.experience') }}
+                                    </th>
+                                    <td>
+                                        {{ $doctor->experience }}
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th>
+                                        {{ trans('cruds.doctor.fields.specialization') }}
+                                    </th>
+                                    <td>
+                                        {{ $doctor->specialization }}
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th>
+                                        {{ trans('cruds.doctor.fields.certifications') }}
+                                    </th>
+                                    <td>
+                                        {{ $doctor->certifications }}
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th>
+                                        {{ trans('cruds.doctor.fields.communication_form') }}
+                                    </th>
+                                    <td>
+                                        {{ $doctor->communication_form }}
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th>
+                                        {{ trans('cruds.doctor.fields.collaborative_communication') }}
+                                    </th>
+                                    <td>
+                                        {{ $doctor->collaborative_communication }}
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th>
+                                        {{ trans('cruds.doctor.fields.states_licensed') }}
+                                    </th>
+                                    <td>
+                                        {{ $doctor->states_licensed }}
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th>
+                                        {{ trans('cruds.doctor.fields.have_malpractice') }}
+                                    </th>
+                                    <td>
+                                        {{ $doctor->have_malpractice }}
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th>
+                                        {{ trans('cruds.doctor.fields.additional_notes') }}
+                                    </th>
+                                    <td>
+                                        {{ $doctor->additional_notes }}
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th>
+                                        {{ trans('cruds.doctor.fields.monthly_stipend') }}
+                                    </th>
+                                    <td>
+                                        {{ $doctor->monthly_stipend }}
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th>
+                                        {{ trans('cruds.doctor.fields.cv') }}
+                                    </th>
+                                    <td>
+                                        @if($doctor->cv)
+                                            <a href="{{ $doctor->cv->getUrl() }}" target="_blank">
+                                                {{ trans('global.view_file') }}
+                                            </a>
+                                        @endif
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <div class="form-group">
+                            <a class="btn btn-default" href="{{ route('frontend.doctors.index') }}">
+                                {{ trans('global.back_to_list') }}
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+@endsection
