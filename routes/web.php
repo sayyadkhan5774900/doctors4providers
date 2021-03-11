513e2d9e660113fffee9c55d52e923146c7fb97d
Laravel Daily <info@laraveldaily.com>
2021-03-11 08:05:57 +0000

    QuickAdminPanel automatic commit

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,18 @@
 <?php
 
-Route::view('/', 'welcome');
+Route::redirect('/', '/login');
+Route::get('/home', function () {
+    if (session('status')) {
+        return redirect()->route('admin.home')->with('status', session('status'));
+    }
+
+    return redirect()->route('admin.home');
+});
+
 Route::get('userVerification/{token}', 'UserVerificationController@approve')->name('userVerification');
 Auth::routes();
 
-Route::group(['prefix' => 'admin', 'as' => 'admin.', 'namespace' => 'Admin', 'middleware' => ['auth', 'admin']], function () {
+Route::group(['prefix' => 'admin', 'as' => 'admin.', 'namespace' => 'Admin', 'middleware' => ['auth']], function () {
     Route::get('/', 'HomeController@index')->name('home');
     // Permissions
     Route::delete('permissions/destroy', 'PermissionsController@massDestroy')->name('permissions.massDestroy');
@@ -79,67 +87,4 @@ Route::group(['prefix' => 'profile', 'as' => 'profile.', 'namespace' => 'Auth', 
         Route::post('profile', 'ChangePasswordController@updateProfile')->name('password.updateProfile');
         Route::post('profile/destroy', 'ChangePasswordController@destroy')->name('password.destroyProfile');
     }
-});
-Route::group(['as' => 'frontend.', 'namespace' => 'Frontend', 'middleware' => ['auth']], function () {
-    Route::get('/home', 'HomeController@index')->name('home');
-
-    // Permissions
-    Route::delete('permissions/destroy', 'PermissionsController@massDestroy')->name('permissions.massDestroy');
-    Route::resource('permissions', 'PermissionsController');
-
-    // Roles
-    Route::delete('roles/destroy', 'RolesController@massDestroy')->name('roles.massDestroy');
-    Route::resource('roles', 'RolesController');
-
-    // Users
-    Route::delete('users/destroy', 'UsersController@massDestroy')->name('users.massDestroy');
-    Route::resource('users', 'UsersController');
-
-    // Doctors
-    Route::delete('doctors/destroy', 'DoctorController@massDestroy')->name('doctors.massDestroy');
-    Route::post('doctors/media', 'DoctorController@storeMedia')->name('doctors.storeMedia');
-    Route::post('doctors/ckmedia', 'DoctorController@storeCKEditorImages')->name('doctors.storeCKEditorImages');
-    Route::resource('doctors', 'DoctorController');
-
-    // Providers
-    Route::delete('providers/destroy', 'ProviderController@massDestroy')->name('providers.massDestroy');
-    Route::post('providers/media', 'ProviderController@storeMedia')->name('providers.storeMedia');
-    Route::post('providers/ckmedia', 'ProviderController@storeCKEditorImages')->name('providers.storeCKEditorImages');
-    Route::resource('providers', 'ProviderController');
-
-    // Agreement Progresses
-    Route::resource('agreement-progresses', 'AgreementProgressController', ['except' => ['create', 'store', 'edit', 'update', 'show', 'destroy']]);
-
-    // Doctor Matches
-    Route::resource('doctor-matches', 'DoctorMatchsController', ['except' => ['create', 'store', 'edit', 'update', 'show', 'destroy']]);
-
-    // Redact Doctor Cvs
-    Route::resource('redact-doctor-cvs', 'RedactDoctorCvController', ['except' => ['create', 'store', 'edit', 'update', 'show', 'destroy']]);
-
-    // Redacted Cvs
-    Route::resource('redacted-cvs', 'RedactedCvController', ['except' => ['create', 'store', 'edit', 'update', 'show', 'destroy']]);
-
-    // Customize Notifications
-    Route::resource('customize-notifications', 'CustomizeNotificationsController', ['except' => ['create', 'store', 'edit', 'update', 'show', 'destroy']]);
-
-    // Doctors Message Approvels
-    Route::resource('doctors-message-approvels', 'DoctorsMessageApprovelsController', ['except' => ['create', 'store', 'edit', 'update', 'show', 'destroy']]);
-
-    // Provider Messages Approvals
-    Route::resource('provider-messages-approvals', 'ProviderMessagesApprovalsController', ['except' => ['create', 'store', 'edit', 'update', 'show', 'destroy']]);
-
-    // Send Payment Links
-    Route::resource('send-payment-links', 'SendPaymentLinkController', ['except' => ['create', 'store', 'edit', 'update', 'show', 'destroy']]);
-
-    // Send Zoom Links
-    Route::resource('send-zoom-links', 'SendZoomLinkController', ['except' => ['create', 'store', 'edit', 'update', 'show', 'destroy']]);
-
-    // Settings
-    Route::delete('settings/destroy', 'SettingController@massDestroy')->name('settings.massDestroy');
-    Route::resource('settings', 'SettingController');
-
-    Route::get('frontend/profile', 'ProfileController@index')->name('profile.index');
-    Route::post('frontend/profile', 'ProfileController@update')->name('profile.update');
-    Route::post('frontend/profile/destroy', 'ProfileController@destroy')->name('profile.destroy');
-    Route::post('frontend/profile/password', 'ProfileController@password')->name('profile.password');
 });
