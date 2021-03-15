8250777bf34a61e2e6521c7463fcb52dc0d8aaf0
sayyadkhan5774900 <59308228+sayyadkhan5774900@users.noreply.github.com>
2021-03-15 22:51:31 +0500

    Create Front End Css File

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -28,6 +28,7 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.5.1/min/dropzone.min.css" rel="stylesheet" />
     <link href="https://cdnjs.cloudflare.com/ajax/libs/jquery.perfect-scrollbar/1.5.0/css/perfect-scrollbar.min.css" rel="stylesheet" />
     <link href="{{ asset('css/custom.css') }}" rel="stylesheet" />
+    <link href="{{ asset('css/frontend.css') }}" rel="stylesheet" />
     @yield('styles')
 </head>
 
