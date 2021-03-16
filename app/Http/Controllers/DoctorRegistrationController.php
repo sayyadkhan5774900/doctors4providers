c0416a38434002be2115657fd4766b4fea4298e6
sayyadkhan5774900 <59308228+sayyadkhan5774900@users.noreply.github.com>
2021-03-16 13:59:16 +0500

    Registration Complete

--- app/Http/Controllers/DoctorRegistrationController.php
+++ app/Http/Controllers/DoctorRegistrationController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Traits\MediaUploadingTrait;
 use App\Models\Doctor;
 use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Http\Request;
@@ -9,8 +10,31 @@ use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 class DoctorRegistrationController extends Controller
 {
+    use MediaUploadingTrait;
 
     public function store(Request $request)
+    {
+        
+        $this->validateData($request);
+
+        $doctor = Doctor::create($request->all());
+
+        if ($request->input('cv', false)) {
+            $doctor->addMedia(storage_path('tmp/uploads/' . basename($request->input('cv'))))->toMediaCollection('cv');
+        }
+
+        if ($request->input('redacted_cv', false)) {
+            $doctor->addMedia(storage_path('tmp/uploads/' . basename($request->input('redacted_cv'))))->toMediaCollection('redacted_cv');
+        }
+
+        if ($media = $request->input('ck-media', false)) {
+            Media::whereIn('id', $media)->update(['model_id' => $doctor->id]);
+        }
+
+        return redirect()->route('landing')->withMessage('Thank you. We will contact you if we have any further questions. When there is a provider who is a good match for you, we will be sure to get in touch with you. We hope to get back to you soon regarding a potential collaboration.');
+    }
+
+    private function validateData(Request $request)
     {
         $request->validate([
             'first_name'                  => [
@@ -56,31 +80,6 @@ class DoctorRegistrationController extends Controller
                 'required',
             ],
         ]);
-
-        $doctor = Doctor::create($request->all());
-
-        if ($request->input('cv', false)) {
-            $doctor->addMedia(storage_path('tmp/uploads/' . basename($request->input('cv'))))->toMediaCollection('cv');
-        }
-
-        if ($request->input('redacted_cv', false)) {
-            $doctor->addMedia(storage_path('tmp/uploads/' . basename($request->input('redacted_cv'))))->toMediaCollection('redacted_cv');
-        }
-
-        if ($media = $request->input('ck-media', false)) {
-            Media::whereIn('id', $media)->update(['model_id' => $doctor->id]);
-        }
-
-        return redirect()->route('frontend.doctors.index');
     }
 
-    public function storeCKEditorImages(Request $request)
-    {
-        $model         = new Doctor();
-        $model->id     = $request->input('crud_id', 0);
-        $model->exists = true;
-        $media         = $model->addMediaFromRequest('upload')->toMediaCollection('ck-media');
-
-        return response()->json(['id' => $media->id, 'url' => $media->getUrl()], Response::HTTP_CREATED);
-    }
 }
